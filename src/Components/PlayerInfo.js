970710fe745ed48e22fab65b0d9d71455975a5f7
BoburDev <86083338+RayimjonovBobur@users.noreply.github.com>
2022-05-19 21:50:16 +0500

    Changed color

--- src/Components/PlayerInfo.js
+++ src/Components/PlayerInfo.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function PlayeInfo(props) {
   const element = props.winner ? (
-    <h1>Winner: {props.winner}</h1>
+    <h1 style={{ color: "#fff" }}>Winner: {props.winner}</h1>
   ) : (
-    <h1>Next player: {props.turn}</h1>
+    <h1 style={{ color: "#fff" }}>Next player: {props.turn}</h1>
   );
   return element;
 }
